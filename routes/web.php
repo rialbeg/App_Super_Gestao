62e0c352936d961ad0a2e6f4222fa53ce5af875f
rialbeg <gsamascarenhas@gmail.com>
2023-10-09 11:14:01 -0300

    feat:created initial controllers

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\SobreNosController;
+use App\Http\Controllers\ContatoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/sobrenos',[SobreNosController::class, 'index']);
+Route::get('/contato', [ContatoController::class, 'index']);
