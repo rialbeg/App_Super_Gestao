c22a321221085a5a9b5c2685f2dfb1067934d60d
rialbeg <gsamascarenhas@gmail.com>
2023-10-09 20:48:14 -0300

    refactor: named the routes

--- routes/web.php
+++ routes/web.php
@@ -17,13 +17,13 @@ use Illuminate\Routing\RouteGroup;
 |
 */
 
-Route::get('/', [HomeController::class, 'index']);
-Route::get('/sobrenos', [SobreNosController::class, 'index']);
-Route::get('/contato', [ContatoController::class, 'index']);
-Route::get('/login', [ContatoController::class, 'index']);
+Route::get('/', [HomeController::class, 'index'])->name('site.index');
+Route::get('/sobrenos', [SobreNosController::class, 'index'])->name('site.sobrenos');
+Route::get('/contato', [ContatoController::class, 'index'])->name('site.contato');
+Route::get('/login', [ContatoController::class, 'index'])->name('site.login');
 
 Route::prefix('app')->group(function () {
-    Route::get('/clientes', [ContatoController::class, 'index']);
-    Route::get('/fornecedores', [ContatoController::class, 'index']);
-    Route::get('/produtos', [ContatoController::class, 'index']);
+    Route::get('/clientes', [ContatoController::class, 'index'])->name('app.clientes');
+    Route::get('/fornecedores', [ContatoController::class, 'index'])->name('app.fornecedores');
+    Route::get('/produtos', [ContatoController::class, 'index'])->name('app.produtos');
 });
